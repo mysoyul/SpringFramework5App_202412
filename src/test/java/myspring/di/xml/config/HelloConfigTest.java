cdafd9ac8044470ad90082dc488c5b204092534e
mysoyul <vega2k_@naver.com>
2024-12-03 08:50:25 +0900

    HelloConfigTest수정

--- src/test/java/myspring/di/xml/config/HelloConfigTest.java
+++ src/test/java/myspring/di/xml/config/HelloConfigTest.java
@@ -1,12 +1,15 @@
 package myspring.di.xml.config;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 import myspring.di.xml.Hello;
+import myspring.di.xml.Printer;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = HelloConfig.class)
@@ -14,8 +17,26 @@ public class HelloConfigTest {
 	@Autowired
 	Hello hello;
 	
+	@Autowired
+	Printer strPrinter;
+	
+	@Autowired
+	@Qualifier("helloCons")
+	Hello hello2;
+	
+	
 	@Test
+	void helloCons() {
+		System.out.println(hello2.sayHello());
+		hello2.print();
+	}
+	
+	@Test @Disabled
 	void helloConfig() {
 		System.out.println(hello.sayHello());
+		hello.print();
+		System.out.println(strPrinter.toString());
 	}
+	
+	
 }

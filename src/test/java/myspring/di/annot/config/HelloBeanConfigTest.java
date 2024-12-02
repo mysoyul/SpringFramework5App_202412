a2bc3a531ffeda04f00b9fa51a013765fe133352
mysoyul <vega2k_@naver.com>
2024-12-02 17:08:29 +0900

    HelloBeanConfigTest 수정

--- src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
+++ src/test/java/myspring/di/annot/config/HelloBeanConfigTest.java
@@ -1,14 +1,27 @@
 package myspring.di.annot.config;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.context.support.AnnotationConfigContextLoader;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import myspring.di.annot.HelloBean;
 
 @ExtendWith(SpringExtension.class)
 //Spring-Test의 AnnotationConfigContextLoader는 AnnotationConfigApplicationContext 컨테이너를 로드해주는 역할
 @ContextConfiguration(classes = HelloBeanConfig.class, loader = AnnotationConfigContextLoader.class)
 public class HelloBeanConfigTest {
-
+	@Autowired
+	HelloBean hello;
+	
+	@Test
+	void helloConfig() {
+		assertEquals("Hello 어노테이션", hello.sayHello());
+	}
+	
+	
 }

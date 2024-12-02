8b553cec1b5e8ccf43a05bac9b64dfe5018f314e
mysoyul <vega2k_@naver.com>
2024-12-02 15:27:44 +0900

    AnnotatedHelloBeanTest assertEquals사용

--- src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
+++ src/test/java/myspring/di/annot/AnnotatedHelloBeanTest.java
@@ -1,11 +1,21 @@
 package myspring.di.annot;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-helloBean.xml")
 public class AnnotatedHelloBeanTest {
-
+	@Autowired
+	HelloBean hello;
+	
+	@Test
+	public void hello() {
+		assertEquals("Hello 어노테이션", hello.sayHello());
+	}
 }

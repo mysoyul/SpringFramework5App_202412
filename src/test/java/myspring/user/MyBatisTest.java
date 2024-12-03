f7b49b5edba3efc2826041d85357720a1b6d2150
mysoyul <vega2k_@naver.com>
2024-12-03 11:49:11 +0900

    SqlSessionTemplate설정

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -6,11 +6,15 @@ import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import myspring.user.vo.UserVO;
 
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(locations = "classpath:spring-beans.xml")
@@ -18,6 +22,19 @@ public class MyBatisTest {
 
 	@Autowired
 	DataSource dataSource;
+	
+	@Autowired
+	SqlSessionFactory sessionFactory;
+	
+	@Autowired
+	SqlSession session;
+	
+	@Test
+	void session() {
+		UserVO user = session.selectOne("userNS.selectUserById", "dooly");
+		System.out.println(user);
+	}
+	
 	
 	@Test
 	void con() {

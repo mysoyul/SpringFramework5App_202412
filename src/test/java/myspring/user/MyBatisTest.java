205ff931595a7a19a174ef2dc95463461312fafb
mysoyul <vega2k_@naver.com>
2024-12-03 10:51:45 +0900

    log4j.xml추가

--- src/test/java/myspring/user/MyBatisTest.java
+++ src/test/java/myspring/user/MyBatisTest.java
@@ -1,9 +1,12 @@
 package myspring.user;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.sql.Connection;
+import java.sql.DatabaseMetaData;
+import java.sql.SQLException;
 
 import javax.sql.DataSource;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -16,5 +19,18 @@ public class MyBatisTest {
 	@Autowired
 	DataSource dataSource;
 	
+	@Test
+	void con() {
+		try {
+			Connection connection = dataSource.getConnection();
+			DatabaseMetaData metaData = connection.getMetaData();
+			System.out.println("DB Url = " + metaData.getURL());
+			System.out.println("DB Username = " + metaData.getUserName());
+			System.out.println("DB ProductName = " + metaData.getDatabaseProductName());
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 	
 }
